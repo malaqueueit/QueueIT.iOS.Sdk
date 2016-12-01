286d2dd16cb1ba87e9e13321290fe063467de209
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2016-12-01 16:41:19 +0100

    Initial Commit

---
+++ QueueItSDK/QueueItSDK.h
@@ -0,0 +1,19 @@
+//
+//  QueueItSDK.h
+//  QueueItSDK
+//
+//  Created by Alexander Yaremchuk on 12/1/16.
+//  Copyright © 2016 Qeueuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QueueItSDK.
+FOUNDATION_EXPORT double QueueItSDKVersionNumber;
+
+//! Project version string for QueueItSDK.
+FOUNDATION_EXPORT const unsigned char QueueItSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueueItSDK/PublicHeader.h>
+
+
